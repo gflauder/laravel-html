5b3a197abcd96683f068264d38e8ec586ade8e88
gflauder <gflauder@hotmail.com>
2019-02-28 04:41:43 -0500

    Update Input.php

--- src/Elements/Input.php
+++ src/Elements/Input.php
@@ -103,9 +103,11 @@ class Input extends BaseElement
     /**
      * @return static
      */
-    public function readonly()
+    public function readonly($readonly = true)
     {
-        return $this->attribute('readonly');
+	    return $readonly
+		    ? $this->attribute('readonly')
+		    : $this->forgetAttribute('readonly');
     }
 
     /**
